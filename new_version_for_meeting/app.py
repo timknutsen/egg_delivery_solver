cdf0dd0791b671fece910a8865e06ef0d523617b
Tim Knutsen <timknutsen@gmail.com>
2025-12-01 14:29:37 +0100

    refactor and add new func

--- new_version_for_meeting/app.py
+++ new_version_for_meeting/app.py
@@ -407,4 +407,4 @@ def export_results(n_clicks, store_data):
 # ==========================================
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8051))
-    app.run_server(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
